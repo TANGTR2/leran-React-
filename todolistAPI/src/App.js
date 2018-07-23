2bebba3a1d01d2745dd298caf75e0c46b66b8f82
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-24 01:44:20 +0800

    Card

--- todolistAPI/src/App.js
+++ todolistAPI/src/App.js
@@ -1,31 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './todo.css';
-import InputContent from './container/InputContentContainer'
-import Todos from './container/TodosContainer'
-import FilterTodo from './container/FilterTodoContainer'
-import {BrowserRouter,Route,Link} from 'react-router-dom'
+import InputContent from './container/InputContentContainer';
+import Todos from './container/TodosContainer';
+import FilterTodo from './container/FilterTodoContainer';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Card } from 'antd';
 
 class App extends Component {
-
-    render() {
-        return (
-            <div className="container">
-                <div>
-                    <h2>Jquery To Do List</h2>
-                    <p>
-                        <em>Simple Todo List with adding and filter by diff status.</em>
-                    </p>
-                </div>
-                <InputContent />   
-                <BrowserRouter>
-                    <div>
-                        <Route exact path="/" component={Todos}></Route>
-                        <Route path="/:status" component={Todos}></Route>
-                    </div>
-                </BrowserRouter>
-                <FilterTodo />
-            </div>);
-    }
+  render() {
+    return (
+      <Card className="container" style={{ align:"cente"}}>
+        <div>
+          <h2>Jquery To Do List</h2>
+          <p>
+            <em>Simple Todo List with adding and filter by diff status.</em>
+          </p>
+        </div>
+        <InputContent />
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Todos} />
+            <Route path="/:status" component={Todos} />
+          </div>
+        </BrowserRouter>
+        <FilterTodo />
+      </Card>
+    );
+  }
 }
 
 export default App;
